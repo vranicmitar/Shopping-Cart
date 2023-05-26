72924272a6f92c35fff5c92dd87b12dab9cb0339
vranicmitar <mitarvranic0@gmail.com>
2023-05-26 19:58:04 +0200

    #update

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -11,12 +11,14 @@ const Cart = () => {
         alt="cart"
       ></img>
       <section style={{ marginLeft: "35%", marginTop: "2%" }}>
-        <h2 style={{ color: "red", fontSize: "30px" }}>
+        <h2 style={{ color: "black", fontSize: "30px" }}>
           Your Shopping Cart is Empty
         </h2>
         <br></br>
         <Link to="/products">
-          <button className="button-89">Start shopping</button>
+          <button className="button-89">
+            <h3>Start shopping</h3>
+          </button>
         </Link>
       </section>
     </div>
